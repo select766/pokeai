550faf53e40549b8df5daf8460ddb28ed02e5789
select766 <select766@outlook.jp>
2019-12-01 11:25:54 +0900

    道具一覧の追加

---
+++ js/tools/enumerate_items.js
@@ -0,0 +1,17 @@
+/*
+持たせられる道具を列挙
+node ./js/tools/enumerate_items.js > data/dataset/all_items.json
+*/
+
+const items = require('../../Pokemon-Showdown/data/items').BattleItems;
+
+const item_ids = [];
+for (const key in items) {
+    if (items.hasOwnProperty(key)) {
+        const element = items[key];
+        if (element.gen === 2) {
+            item_ids.push(key);
+        }
+    }
+}
+process.stdout.write(JSON.stringify(item_ids, null, 2));
