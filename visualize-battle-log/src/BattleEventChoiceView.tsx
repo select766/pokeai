ea6827183d86ff4a5af2bd19679d1c3c30a9f133
select766 <select766@outlook.jp>
2020-12-12 20:26:34 +0900

    q値の可視化

--- visualize-battle-log/src/BattleEventChoiceView.tsx
+++ visualize-battle-log/src/BattleEventChoiceView.tsx
@@ -12,7 +12,7 @@ export function BattleEventChoiceView({ event }: BattleEventChoiceViewProps): Re
   }
   const line1 = [<td key="player">{event.choice.player}</td>];
   const line2 = [<td key="player"></td>];
-  
+
   const sideActiveStatus = event.choice.battle_status.side_statuses[event.choice.battle_status.side_friend].active;
   let sidePokeStatus = `${sideActiveStatus.hp_current}/${sideActiveStatus.hp_max} ${sideActiveStatus.status} ${sideActiveStatus.volatile_statuses.join(' ')}`;
   for (const key of Object.keys(sideActiveStatus.ranks)) {
@@ -21,17 +21,19 @@ export function BattleEventChoiceView({ event }: BattleEventChoiceViewProps): Re
       sidePokeStatus += ` ${key}${rank > 0 ? '+' : ''}${rank}`;
     }
   }
-  for (const action of event.choice.possible_actions) {
+  for (let i = 0; i < event.choice.possible_actions.length; i++) {
+    const action = event.choice.possible_actions[i];
+    const q_value = event.choice.q_func?.q_func[i]?.toFixed(2) || '';
     if (action.switch) {
       const movesJp = action.allMoves.map(name2jp);
-      line1.push(<td key={action.simulator_key} className={action.simulator_key === event.choice.choice ? 'actualChoice' : ''}>{`${name2jp(action.poke)}`}</td>);
-      line2.push(<td key={action.simulator_key}>{movesJp[0]},{movesJp[1]}<br/>{movesJp[2]},{movesJp[3]}</td>);
+      line1.push(<td key={action.simulator_key} className={action.simulator_key === event.choice.choice ? 'actualChoice' : ''}>{`${name2jp(action.poke)} ${q_value}`}</td>);
+      line2.push(<td key={action.simulator_key}>{movesJp[0]},{movesJp[1]}<br />{movesJp[2]},{movesJp[3]}</td>);
     } else {
       if (action.simulator_key === 'move 1') {
-        // TODO: 技が4つでない場合
-        line1.push(<td key={action.simulator_key} colSpan={4}>{`${name2jp(action.poke)} ${sidePokeStatus}`}</td>);
+        const n_moves = event.choice.possible_actions.filter((v) => !v.switch).length;
+        line1.push(<td key={action.simulator_key} colSpan={n_moves}>{`${name2jp(action.poke)} ${sidePokeStatus}`}</td>);
       }
-      line2.push(<td key={action.simulator_key} className={action.simulator_key === event.choice.choice ? 'actualChoice' : ''}>{`${name2jp(action.move)}`}</td>);
+      line2.push(<td key={action.simulator_key} className={action.simulator_key === event.choice.choice ? 'actualChoice' : ''}>{`${name2jp(action.move)}`}<br />{q_value}</td>);
     }
   }
   return <div className="battleEventChoiceView">
